38938adeb2900a6fc88bfd00103459dece356448
Russell Ripley <russellripley@hotmail.com>
2015-12-01 21:11:41 -0700

    fixed thread layout

---
+++ public/make_thread.php
@@ -0,0 +1,17 @@
+<?php require('header.php'); ?>
+
+
+<form>
+	Title:
+	<input type="text">
+	<br>
+
+		<textarea>
+			Enter text here...
+		</textarea>
+
+	<br>
+	<input type="submit">
+</form>
+
+<?php require('footer.php'); ?>
--- public/thread.php
+++ public/thread.php
@@ -4,16 +4,13 @@
 <form action="post_reply.php" method="post">
 	Title:
 	<input type="text">
-	<br>
+<h1>
+	This is the title of the thread.
+</h1>
 
-		<textarea>
-			Enter text here...
-		</textarea>
-
-	<br>
-	<input type="submit">
-</form>
-
+<div class="thread-post">
+	This will be where the thread post will go. The first thread post from the thread author will be the first post on the page.
+</div>
 	<br>
 
 <form>

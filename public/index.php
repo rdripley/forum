5da9140975e7d4396a91d7e4263b4fff0008520e
Russ Ripley <russellripley@hotmail.com>
2015-12-05 09:07:57 -0700

    fixed layout on home page

--- public/index.php
+++ public/index.php
@@ -4,72 +4,40 @@
 	<input type="submit" value="Make a New Thread">
 </form>
 
-<table>
-	<tr>
-		<td>Popular</td>
-		<td>Date</td>
-		<td>
-			<a href="view_thread.php">Post Title</a>
-		</td>
-	</tr>
+<section>
+	<table class="thread-table">
+		<tr>
+			<td>Popular</td>
+			<td>Date</td>
+			<td>
+				<a href="view_thread.php">Post Title</a>
+			</td>
+		</tr>
 
-	<tr>
-		<td>Paul</td>
-		<td>9/1/2001</td>
-		<td>
-			<a href="view_thread.php">Sample Post</a>
-		</td>
-	</tr>
+		<tr>
+			<td>Paul</td>
+			<td>9/1/2001</td>
+			<td>
+				<a href="view_thread.php">Sample Post</a>
+			</td>
+		</tr>
 
-	<tr>
-		<td>Russ</td>
-		<td>10/10/2010</td>
-		<td>
-			<a href="view_thread.php">More Sample Stuff</a>
-		</td>
-	</tr>
+		<tr>
+			<td>Russ</td>
+			<td>10/10/2010</td>
+			<td>
+				<a href="view_thread.php">More Sample Stuff</a>
+			</td>
+		</tr>
 
-	<tr>
-		<td>Paul Russ</td>
-		<td>9/10/2001</td>
-		<td>
-			<a href="view_thread.php">Combine Fake Data</a>
-		</td>
-	</tr>
-</table>
-
-<table>
-	<tr>
-		<td>My Topics</td>
-		<td>Date</td>
-		<td>
-			<a href="view_thread.php">Post Title</a>
-		</td>
-	</tr>
-
-	<tr>
-		<td>Paul</td>
-		<td>9/1/2001</td>
-		<td>
-			<a href="view_thread.php">Sample Post</a>
-		</td>
-	</tr>
-
-	<tr>
-		<td>Russ</td>
-		<td>10/10/2010</td>
-		<td>
-			<a href="view_thread.php">More Sample Stuff</a>
-		</td>
-	</tr>
-
-	<tr>
-		<td>Paul Russ</td>
-		<td>9/10/2001</td>
-		<td>
-			<a href="view_thread.php">Combine Fake Data</a>
-		</td>
-	</tr>
-</table>
+		<tr>
+			<td>Paul Russ</td>
+			<td>9/10/2001</td>
+			<td>
+				<a href="view_thread.php">Combine Fake Data</a>
+			</td>
+		</tr>
+	</table>
+</section>
 
 <?php require('footer.php'); ?>

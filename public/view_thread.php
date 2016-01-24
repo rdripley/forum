2336c7c3bb5443490cc205b0d1d9a743e1ba087a
Russell Ripley <russellripley@hotmail.com>
2016-01-24 10:49:12 -0700

    finish wiring up thread functionality

--- public/view_thread.php
+++ public/view_thread.php
@@ -4,26 +4,30 @@
 
 require('database.php');
 
+$threadId = $_GET['id'];
+
 $db = new Database();
-$threadsquery = "SELECT threads.*, users.username as author_name FROM threads" .
-	"JOIN users ON users.id = threads.author_id" .
-	"WHERE threads.id = 1";
+$threadsQuery = "SELECT threads.*, users.username as author_name FROM threads " .
+	"JOIN users ON users.id = threads.author_id " .
+	"WHERE threads.id = $threadId";
 
 
-$postsquery = "SELECT posts.*, users.username as author_name FROM `posts`" .
-	"JOIN users ON users.id = posts.author_id" .
-	"WHERE posts.thread_id = 1"
+$postsQuery = "SELECT posts.*, users.username as author_name FROM `posts` " .
+	"JOIN users ON users.id = posts.author_id " .
+	"WHERE posts.thread_id = $threadId " .
 	"ORDER BY posts.date ASC";
 
+$posts = array();
 
-/*I'm not sure what to change for the rest of this PHP code */
-$threads = array();
+$postResults = $db->query($postsQuery);
 
-$results = $db->query($threadsquery);
-
-while ($result = $results->fetch_assoc()) {
-	array_push($threads, $result);
+while ($result = $postResults->fetch_assoc()) {
+	array_push($posts, $result);
 }
+
+$threadResults = $db->query($threadsQuery);
+
+$thread = $threadResults->fetch_assoc();
 
 ?>
 
@@ -39,40 +43,38 @@ while ($result = $results->fetch_assoc()) {
 
 <hr>
 
-<div class="post">
-	<p class="post_details">
-		<?= $thread['author_name']; ?> | <?= date('n/j/Y H:i', strtotime($thread['date'])); ?>
-	</p>
+<?php
+	foreach($posts as $post) { ?>
+	<div class="post">
+		<p class="post_details">
+			<?= $post['author_name']; ?> | <?= date('n/j/Y H:i', strtotime($post['date'])); ?>
+		</p>
 
-	<p>
-		<?= $thread['title']; ?>
-	</p>
+		<p>
+		<?= $post['content']; ?>
+		</p>
 
-	<p>
-		First you take some apples and you put them in a pie crust. Then you put it in the oven on 400 degrees and bake it until the pie's done.
-	</p>
-</div>
-
-<div class="post">
-	<p class ="post_details">
-		RDRipley | Mon Nov 30, 2015 11:51 AM
-	</p>
-
-	<p>
-		Dude, that's nothing. My wife's pie is way better.
-	</p>
-</div>
+		<button type="button">
+			Delete
+		</button>
+	</div>
+	<?php } ?>
 
 <br>
 
-<form action="reply.php">
-	<textarea placeholder="Enter your reply here..."></textarea>
+<?php
+	if ($_SESSION['user']) { ?>
+		<form action="reply.php" method="POST">
+			<input type="hidden" name="thread_id" value="<?= $threadId;?>">
+			<textarea name="content" placeholder="Enter your reply here..."></textarea>
 
-	<br>
+			<br>
 
-	<button type="submit">
-		Submit
-	</button>
-</form>
+			<button type="submit">
+				Submit
+			</button>
+		</form>
+
+	<?php } ?>
 
 <?php require('footer.php'); ?>
